080b7db05da4a6c6726e269e2603364ffeb4f055
vimalraj-tecdata <vimalraj@tecdata.co.in>
2024-08-10 17:14:44 +0530

    first commit

---
+++ src/main/java/com/example/jenkin/api/DockerJenkinsDemoIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsDemoIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsDemoIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkin/api/DockerJenkinsDemoIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsDemoIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
